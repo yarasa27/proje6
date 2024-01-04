f698b7d31e5847615059deaef867f3c574dd3c38
yarasa27 <elcan.abbasoff@gmail.com>
2024-01-05 01:49:27 +0400

    gerisi

---
+++ script6.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+
+search.addEventListener('click', () => {
+    const APIKey = 'a935a1c384ecd10bb7843e02235dcf2a';
+    const city = document.querySelector('.search-box input').value;
+    if(city == '')
+        return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
+        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-details .humidity span');
+        const wind = document.querySelector('.weather-details .wind span');
+
+        switch (json.weather[0].main) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+
+
+    });
+
+});
